743b625344443ce1f24c4a170b2bab8cf7d08324
dseijasiglesias <dseijasiglesias@danielcastelao.org>
2016-02-24 11:53:10 +0100

    First Commit

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name':'El Tema de Seijas',
+    'description': 'El mejor tema, para el mejor odoo',
+    'version':'1.0',
+    'author':'Daniel Seijas',
+    
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml'
+    ],
+    'category': 'Theme/Creative',
+    'depends': ['website'],
+}
